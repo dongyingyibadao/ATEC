c312ded3dbad68a43a781aa1ab17e269ca882de8
dongyingyibadao <2974127927@qq.com>
2026-06-29 15:18:54 +0000

    new_change

--- demo/solution.py
+++ demo/solution.py
@@ -1,4 +1,1 @@
-try:
-    from .my_refactored.entry import AlgSolution  # noqa: F401
-except ImportError:
-    from my_refactored.entry import AlgSolution  # noqa: F401
+from .my_submission.entry import AlgSolution  # noqa: F401
